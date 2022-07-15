e0441184f27c374e68422a78be6c155506e3108e
Phuong Le <phuongl@amperecomputing.com>
2022-07-15 09:11:17 +0700

    oke

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #define DHTPIN 27 // Digital pin connected to the DHT sensor
 #define servosPins 26
 #define DHTTYPE DHT11 // DHT 11
-#define MOTIONSENSOR_PIN 1
+#define MOTIONSENSOR_PIN 14
 #define BUTTON_PIN 25
 // #define DHTTYPE DHT22 // DHT 22 (AM2302)
 const char *ssid = "Fuvitech";
@@ -20,18 +20,17 @@ const char *password = "fuvitech.vn";
 #define MQTT_PORT 1883
 #define MQTT_TEMPERATURE_TOPIC "nodeWiFi32/dht11/temperature"
 #define MQTT_HUMIDITY_TOPIC "nodeWiFi32/dht11/humidity"
-#define MQTT_LED_TOPIC "nodeWiFi32/led"
-
+#define MQTT_HEARBEAT_TOPIC "heartbeat"
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // 0x27 địa chỉ LCD, 16 cột và 2 hàng
 const byte ROWS = 4;                // Bốn hàng
-const byte COLS = 4;                // Ba cột
+const byte COLS = 3;                // Ba cột
 byte rowPins[ROWS] = {19, 18, 5, 17};
-byte colPins[COLS] = {16, 4, 0, 2};
+byte colPins[COLS] = {16, 4, 0};
 char keys[ROWS][COLS] = {
-    {'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9','C'}, {'*', '0', '#','D'}};
+    {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}};
 
 char STR[4] = {'2', '0', '1', '8'}; // Cài đặt mật khẩu tùy ý
 char str[4] = {' ', ' ', ' ', ' '};
@@ -74,12 +73,12 @@ void setup()
   lcd.backlight();
   lcd.clear();
   lcd.print(" Enter Password");
+  Serial.println("create task...");
+  xTaskCreate(Task1code, "Task1", 20000, NULL, 1, &Task1);
+  vTaskDelay(500);
 
-  xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 1, &Task1, 0);
-  delay(500);
-
-  xTaskCreatePinnedToCore(Task2code, "Task2", 10000, NULL, 1, &Task2, 1);
-  delay(500);
+  xTaskCreate(Task2code, "Task2", 20000, NULL, 1, &Task2);
+  vTaskDelay(500);
 }
 void Task1code(void *parameter)
 {
@@ -102,14 +101,14 @@ void Task1code(void *parameter)
       if (isnan(h) || isnan(t))
       {
         Serial.println(F("Failed to read from DHT sensor!"));
-        return;
       }
       else
       {
+        Serial.printf("Temperature: %f\n", t);
         client.publish(MQTT_TEMPERATURE_TOPIC, String(t).c_str());
         client.publish(MQTT_HUMIDITY_TOPIC, String(h).c_str());
-        previousMillis = currentMillis;
       }
+      previousMillis = currentMillis;
     }
   }
 }
@@ -121,55 +120,66 @@ void Task2code(void *parameter)
 
   for (;;)
   {
-    if (digitalRead(MOTIONSENSOR_PIN) == HIGH)
+    if (digitalRead(MOTIONSENSOR_PIN))
     {
-      if (doorState == DOOR_STATE_OPEN)
+      delay(100);
+      if (digitalRead(MOTIONSENSOR_PIN))
       {
-        closeDoor();
+
+        if (doorState == DOOR_STATE_OPEN)
+        {
+          closeDoor();
+        }
+        else
+        {
+          openDoor();
+        }
       }
-      else
-      {
-        openDoor();
-      }
+      
     }
     char key = keypad.getKey(); // Ký tự nhập vào sẽ gán cho biến Key
     if (key)                    // Nhập mật khẩu
     {
+      Serial.println(key);
       if (i == 0)
       {
         str[0] = key;
-        lcd.setCursor(6, 1);
-        lcd.print(str[0]);
-        delay(1000); // Ký tự hiển thị trên màn hình LCD trong 1s
-        lcd.setCursor(6, 1);
-        lcd.print("*"); // Ký tự được che bởi dấu *
+        // lcd.setCursor(6, 1);
+        // lcd.print(str[0]);
+        // delay(1000); // Ký tự hiển thị trên màn hình LCD trong 1s
+        // lcd.setCursor(6, 1);
+        // lcd.print("*"); // Ký tự được che bởi dấu *
+        Serial.print("*");
       }
       if (i == 1)
       {
         str[1] = key;
-        lcd.setCursor(7, 1);
-        lcd.print(str[1]);
-        delay(1000);
-        lcd.setCursor(7, 1);
-        lcd.print("*");
+        // lcd.setCursor(7, 1);
+        // lcd.print(str[1]);
+        // delay(1000);
+        // lcd.setCursor(7, 1);
+        // lcd.print("*");
+        Serial.print("*");
       }
       if (i == 2)
       {
         str[2] = key;
-        lcd.setCursor(8, 1);
-        lcd.print(str[2]);
-        delay(1000);
-        lcd.setCursor(8, 1);
-        lcd.print("*");
+        // lcd.setCursor(8, 1);
+        // lcd.print(str[2]);
+        // delay(1000);
+        // lcd.setCursor(8, 1);
+        // lcd.print("*");
+        Serial.print("*");
       }
       if (i == 3)
       {
         str[3] = key;
-        lcd.setCursor(9, 1);
-        lcd.print(str[3]);
-        delay(1000);
-        lcd.setCursor(9, 1);
-        lcd.print("*");
+        // lcd.setCursor(9, 1);
+        // lcd.print(str[3]);
+        // delay(1000);
+        // lcd.setCursor(9, 1);
+        // lcd.print("*");
+        Serial.print("*");
         count = 1;
       }
       i = i + 1;
@@ -180,25 +190,27 @@ void Task2code(void *parameter)
       if (str[0] == STR[0] && str[1] == STR[1] && str[2] == STR[2] &&
           str[3] == STR[3])
       {
-        lcd.clear();
-        lcd.print("    Correct!");
-        delay(3000);
+        // lcd.clear();
+        // lcd.print("    Correct!");
+        // delay(3000);
         myServo.write(180); // Mở cửa
-        lcd.clear();
-        lcd.print("      Opened!");
+        Serial.println("Opened Door");
+        // lcd.clear();
+        // lcd.print("      Opened!");
         i = 0;
         count = 0;
       }
       else
       {
-        lcd.clear();
-        lcd.print("   Incorrect!");
-        delay(3000);
-        lcd.clear();
-        lcd.print("   Try Again!");
-        delay(3000);
-        lcd.clear();
-        lcd.print(" Enter Password");
+        Serial.println("Try again!");
+        // lcd.clear();
+        // lcd.print("   Incorrect!");
+        // delay(3000);
+        // lcd.clear();
+        // lcd.print("   Try Again!");
+        // delay(3000);
+        // lcd.clear();
+        // lcd.print(" Enter Password");
         i = 0;
         count = 0;
       }
@@ -207,19 +219,32 @@ void Task2code(void *parameter)
     switch (key)
     {
     case '#':
-      lcd.clear();
+      Serial.println("Closed!");
+      // lcd.clear();
       myServo.write(90); // close door
-      lcd.print("     Closed!");
-      delay(10000);
-      lcd.clear();
-      lcd.print(" Enter Password");
+      // lcd.print("     Closed!");
+      // delay(10000);
+      // lcd.clear();
+      // lcd.print(" Enter Password");
       i = 0;
+      break;
+    case '*':
+      Serial.printf("doorState:%d\n",doorState);
+        if (doorState == DOOR_STATE_OPEN)
+        {
+          closeDoor();
+        }
+        else
+        {
+          openDoor();
+        }
       break;
     }
   }
 }
 void loop()
 {
+  vTaskDelay(2000);
 }
 
 void setup_wifi()
@@ -229,7 +254,7 @@ void setup_wifi()
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    vTaskDelay(500);
     Serial.print(".");
   }
   randomSeed(micros());
@@ -245,7 +270,7 @@ void connect_to_broker()
   {
     Serial.print("Attempting MQTT connection...");
     String clientId = "fuvitech";
-    if (client.connect(clientId.c_str()))
+    if (client.connect("fuvitech"))
     {
       Serial.println("connected");
     }
@@ -254,7 +279,7 @@ void connect_to_broker()
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 2 seconds");
-      delay(2000);
+      vTaskDelay(2000);
     }
   }
 }
